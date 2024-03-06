238778ea00175a5523a6f311cc2459f3fad28c8b
Morelosa <Antoniomorelos2003@gmail.com>
2024-03-06 16:59:11 -0600

    Created new FMS Test

---
+++ Backend/inlinelunge.py
@@ -0,0 +1,8 @@
+#Testing inline Lunge in openCV and mediapipe
+#2 Angles required: 1 from the side, and one from the front
+#Author of this page: Antonio Morelos
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
