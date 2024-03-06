669b2ff1a437e153940e4b072f74a16ba353cd0c
Morelosa <Antoniomorelos2003@gmail.com>
2024-03-06 16:29:54 -0600

    Tested the react-flask connection

--- Frontend/fmsv2/src/App.js
+++ Frontend/fmsv2/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+//import Test from './Pages/Test.js';
+
 
 function App() {
   return (
@@ -23,3 +25,18 @@ function App() {
 }
 
 export default App;
+
+
+//The following commented code was to test the connectivity of flask to react.
+//It was left here as refrence. For info about the <Test/> page, look in the Pages folder
+
+/*
+function App(){
+  return(
+    <div className = "App">
+      <Test/>
+    </div>
+  );
+}
+export default App;
+*/
