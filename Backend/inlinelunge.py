ab9124e414dd536a9e9c97ae5ef2e8debdc99d5b
Morelosa <Antoniomorelos2003@gmail.com>
2024-03-13 02:06:42 -0500

    Brainstorming for Inlinelunge

--- Backend/inlinelunge.py
+++ Backend/inlinelunge.py
@@ -2,6 +2,38 @@
 #2 Angles required: 1 from the side, and one from the front
 #Author of this page: Antonio Morelos
 
+
+#Conditions for proper score:
+# Minimal torso movement (Technically a stick is supposed to be involved but torso movement does the same)
+# Feet must remain in sagital plane
+# Knee must touch floor
+# Front foot remains in place
+
+
+# Must track: Knee to floor,Front foot, torso angle
+
+#For torso
+#1 Calculate angle between 11-23 and 23-25
+#Check to make sure it doesn't move much
+#Be sure to take the INITIAL torso angle as a refrenc
+
+#For Front Foot
+#Make sure it does not move. That is all  (Ensure  30-32 angle does not move)
+
+#For knee
+#TBD
+
+
+##Ideas: To calculate the joint angles, just save related markers to an array for each of the markers
+##Processing will take place AFTER the data collection
+##In order to find the FMS value, take the standard deviation, insuring you compare it to the INITIAL
+## pose position, & return from there
+##In order to retrieve the initial values to compare the standard deviation, just take the first daatapoint in the array
+
+##For this  exercie, you will need to get three instanes of standard deviation: Front foot movement,Torso movement, and
+##Knee to floor (SOMEHOW IDK it's not related to standard deviation)
+
+
 import cv2
 import mediapipe as mp
 
